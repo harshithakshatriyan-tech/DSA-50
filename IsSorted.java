a989c5af7ea26d913ebba02d6f6135a45f73103c
hixz51 <harshithakshatriyan@gmail.com>
2026-04-05 10:00:00 +0530

    DSA 50

---
+++ IsSorted.java
@@ -0,0 +1,15 @@
+public class IsSorted {
+    public static boolean checkSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Is Sorted: " + checkSorted(arr));
+    }
+}
