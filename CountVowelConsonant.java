b5bdccfe9b1a4487348cbfca3d4d5e7d1b7600a5
hixz51 <harshithakshatriyan@gmail.com>
2026-04-09 10:00:00 +0530

    DSA 50

---
+++ CountVowelConsonant.java
@@ -0,0 +1,25 @@
+public class CountVowelConsonant {
+
+    public static void count(String str) {
+        int vowels = 0, consonants = 0;
+
+        str = str.toLowerCase();
+
+        for (char ch : str.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                if ("aeiou".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+        count("hello");
+    }
+}
